c0ff05c5c41ebf151483f7e4a5cc2be9c7635a0a
mnitek <maciej.nitek@gmail.com>
2021-03-25 15:02:13 +0100

    Added WebClient

--- src/main/java/io/github/mnitek/servit/configuration/Config.java
+++ src/main/java/io/github/mnitek/servit/configuration/Config.java
@@ -1,8 +1,20 @@
 package io.github.mnitek.servit.configuration;
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import reactor.netty.http.client.HttpClient;
+
+import javax.net.ssl.SSLException;
+
 
 @Configuration
 public class Config implements WebMvcConfigurer {
@@ -10,5 +22,24 @@ public class Config implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
         //registry.addViewController("/login");
+    }
+
+    @Bean
+    public WebClient webClient() throws SSLException {
+        SslContext sslContext = SslContextBuilder
+                .forClient()
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .build();
+
+        HttpClient httpClient = HttpClient.create().secure(t -> t.sslContext(sslContext));
+
+                String token = "Bearer 518f2340684199ab4d745ca2fbb7a2067079fccb";
+
+        return WebClient.builder()
+                .baseUrl("https://developer.todoist.com/rest/v1")
+                //.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, token)
+                .build();
+
     }
 }
--- src/main/java/io/github/mnitek/servit/controller/IngredientController.java
+++ src/main/java/io/github/mnitek/servit/controller/IngredientController.java
@@ -1,7 +1,8 @@
 package io.github.mnitek.servit.controller;
 
-//import io.github.mnitek.servit.logic.IngredientService;
+import io.github.mnitek.servit.logic.IngredientService;
 import io.github.mnitek.servit.model.Ingredient;
+import io.github.mnitek.servit.model.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,23 +10,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-/*
+
 @Controller
 @RequestMapping("/ingredients")
 public class IngredientController {
     private IngredientService service;
 
-    @GetMapping
-    public Flux<Ingredient> getAllIngredients () {
-        return service.getAllIngredients();
+    IngredientController(final IngredientService service) {
+        this.service = service;
     }
 
-    @PostMapping
-    public Mono<Ingredient> createIngredientsTasks(@RequestBody Ingredient ingredient) {
-        return service.createIngredientsTasks(ingredient);
-    }
+    //    @PostMapping
+//    public Mono<Ingredient> createIngredientTask(@RequestBody Ingredient ingredient) {
+//        return service.createIngredientTask(ingredient);
+//    }
 
+    @GetMapping("/tasks")
+    public Flux<Task> getTasks() {
+        return service.getAllTasks();
+    }
 
 }
 
- */
