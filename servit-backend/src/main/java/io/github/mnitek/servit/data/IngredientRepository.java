21f0c2b6409b4577b42d2c801323925c3ddd3742
maciej <maciej.nitek@ncdc.pl>
2021-11-30 16:19:30 +0100

    [IP-11] Deleted sql repositories moved methods to repositories

--- servit-backend/src/main/java/io/github/mnitek/servit/data/IngredientRepository.java
+++ servit-backend/src/main/java/io/github/mnitek/servit/data/IngredientRepository.java
@@ -1,14 +1,23 @@
 package io.github.mnitek.servit.data;
 
 import io.github.mnitek.servit.model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface IngredientRepository {
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
 
     Ingredient save(Ingredient ingredient);
-    List<Ingredient> findAllPlanned();
-    List<Ingredient> findAllPlannedOrderByName();
+
     Optional<Ingredient> findById(int id);
+
+    @Query("select distinct i from Ingredient i where i.planned=true ")
+    List<Ingredient> findAllPlanned();
+
+    @Query("select distinct i from Ingredient i where i.planned=true order by i.name")
+    List<Ingredient> findAllPlannedOrderByName();
 }
